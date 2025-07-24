10e3c8c47fb115cd6adbb90cb127af075de5541a
AntonioIIOlaguer <antonio.olaguer.ii@gmail.com>
2025-07-23 22:26:13 -0400

    Fixed import statements

--- services/db.py
+++ services/db.py
@@ -1,6 +1,7 @@
-from models.User import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+from services.models.models import Base
 
 DATABASE_URL = "sqlite:///vaults.db"
 engine = create_engine(DATABASE_URL, echo=False)
